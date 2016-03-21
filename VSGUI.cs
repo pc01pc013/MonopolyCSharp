5b44d25017694cebea780f72b6234a4cd678d299
pc01pc013 <pc01pc013@gmail.com>
2016-03-22 01:18:57 +0800

    初始建置版本

---
+++ VSGUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonopolyCSharp
+{
+    public partial class VSGUI : Form
+    {
+        public VSGUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
